67c4344e8574e9aa300509e024416a77a96e2353
BuddhaL1T3 <jmriggs75@gmail.com>
2018-05-14 16:38:07 -0400

    added several methods

--- scripts/Store.js
+++ scripts/Store.js
@@ -9,9 +9,51 @@ const store = (function() {
         ]
         const hideCheckedItems = false;
         const searchTerm = '';
-      return {
+        
+        function findById(id) {
+            return store.items.find(item => item.id === id);
+        }
+        
+        function addItem(name) {
+            try {
+                Item.validateName(name);
+                Item.create(name);
+                this.items.push({id: cuid(), name, checked: false})
+            }
+            catch(e) {
+                console.log('Cannot add item: ' + e.message);
+            }
+        }
+
+        function findAndToggleChecked(id) {
+            this.findById(id).toggleClass('.checked');
+        }
+        
+        function findAndUpdateName(id, newName) {
+            try {
+                Item.validateName(newName);
+                Item.findById(id);
+                this.items.push({id: cuid(), newName, checked: false})
+            }
+            catch(e) {
+                console.log('Cannot update name: ' + e.message);
+            }
+        }
+
+        function findAndDelete(id) {
+            this.items = this.items.filter(item => {
+                return item.id !== id;
+            });
+        }
+      
+        return {
           items,
           hideCheckedItems,
           searchTerm,
+          findAndDelete,
+          findAndToggleChecked,
+          findAndUpdateName,
+          findById,
+          addItem,
       }
 }());
